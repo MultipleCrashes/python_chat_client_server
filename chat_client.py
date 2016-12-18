b5d326f70384fe4a8dacc409b3597257df9a050e
harish_upadhyay <inimitableharish@gmail.com>
2016-12-18 13:44:00 +0530

    added check for registered user

--- chat_client.py
+++ chat_client.py
@@ -2,6 +2,22 @@ import sys
 import socket 
 import select 
 
+
+def mongo_connect(): # TBD make it parameterized 
+    from pymongo import MongoClient
+    client = MongoClient()
+    return client    #Db handle  
+    
+
+def check_registered(mobile_number='8123340865'):
+    db_client = mongo_connect()
+    database_connection = db_client['whatsapp']  # db instance 
+    collection_doc = database_connection.user.find({"mobile_number":mobile_number}).count()
+    if collection_doc > 0:
+        return True          
+    else:
+        return False    # User not registered 
+
 def chat_client():
 	if(len(sys.argv) < 3):
 		print "Usage : python chat_client.py hostname port"
@@ -9,7 +25,7 @@ def chat_client():
 
 	host = sys.argv[1]
 	port = int(sys.argv[2])
-        print "client connecting to host %s and port %s ",host , port
+        print "client connecting to host and port ",host ,port
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.settimeout(2)
 	try:
@@ -19,7 +35,13 @@ def chat_client():
 		print "unable to connect"
 		sys.exit()
 
-        user_phone_number=str(raw_input("enter your 10 digit phone number"))
+        user_phone_number=str(raw_input("enter your 10 digit phone number : "))
+        while(len(user_phone_number)!=10):   # basic check 
+            print "please enter 10 digit mobile number"
+            user_phone_number = str(raw_input("enter your 10 digit phone number : "))
+        while not check_registered(user_phone_number):
+            user_phone_number = str(raw_input("previous user was not registerd please enter your 10 digit phone number : "))
+
         user_phone_number = user_phone_number + " : "
 	while 1:
 		socket_list = [sys.stdin,s ]
